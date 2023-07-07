5706ea5d4c2172191c6926cf089338a779943133
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2023-07-07 19:46:51 +0530

    final commit to this project

--- contact_tracker/src/routes/destroy.jsx
+++ contact_tracker/src/routes/destroy.jsx
@@ -3,7 +3,6 @@ import { deleteContact } from "../contacts";
 
 
 export async function action ({ params }) {
-    throw new Error("oh dang!");
     await deleteContact(params.contactId);
     return redirect('/');
 }
